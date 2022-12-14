632a1512dbec82bf3e90490e1afeeb8ed192c430
pechev <pechev95@gmail.com>
2022-12-14 20:39:59 +0200

    Add exercise Numbers 1 to 100

---
+++ SoftUni/Java/Basics/03. For Loop/Lab/01. Numbers1To100.java
@@ -0,0 +1,11 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class Numbers1To100 {
+    public static void main(String[] args) {
+        for (int number = 1; number < 100; number++) {
+            System.out.println(number);
+        }
+    }
+}
