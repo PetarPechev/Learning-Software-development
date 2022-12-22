1d2bc215219083acb33e6f3b5f20e84ec4c8f086
pechev <pechev95@gmail.com>
2022-12-22 23:24:15 +0200

    Add exercise Town Info

---
+++ SoftUni/Java/Fundamentals/02. Data Types and Variables/Lab/04. TownInfo.java
@@ -0,0 +1,15 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class TownInfo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String cityName = scanner.nextLine();
+        int population = Integer.parseInt(scanner.nextLine());
+        int citySquareArea = Integer.parseInt(scanner.nextLine());
+
+        System.out.printf("Town %s has population of %d and area %d square km.", cityName, population, citySquareArea);
+    }
+}
