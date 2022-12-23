6f09a42c0e7e1da0435ce3c634c344c1a88bf8a9
pechev <pechev95@gmail.com>
2022-12-23 06:44:03 +0200

    Add exercise Printer part of the ASCII table

---
+++ SoftUni/Java/Fundamentals/02. Data Types and Variables/Exercise/05. PrintPartOfTheASCIITable.java
@@ -0,0 +1,16 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class PrintPartOfTheASCIITable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startIndex = Integer.parseInt(scanner.nextLine());
+        int endIndex = Integer.parseInt(scanner.nextLine());
+
+        for (int i = startIndex; i <= endIndex; i++) {
+            System.out.print((char)i + " ");
+        }
+    }
+}
