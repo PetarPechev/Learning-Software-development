332b531398b2d2f8436edbfb10731bcfbf5c50de
pechev <pechev95@gmail.com>
2022-12-27 00:26:40 +0200

    Add exercise Even and Odd Subtraction

---
+++ SoftUni/Java/Fundamentals/03. Arrays/Lab/05. EvenAndOddSubtraction.java
@@ -0,0 +1,28 @@
+package Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EvenAndOddSubtraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbersArray = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int number : numbersArray) {
+            if (number % 2 == 0){
+                evenSum += number;
+            } else {
+                oddSum += number;
+            }
+        }
+
+        System.out.print(evenSum - oddSum);
+    }
+}
