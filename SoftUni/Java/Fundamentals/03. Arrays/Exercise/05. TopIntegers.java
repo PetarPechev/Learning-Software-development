bb3fb5180969204120d30d0a7d072ba9fc06bc35
pechev <pechev95@gmail.com>
2022-12-27 20:30:47 +0200

    Add exercise Top integers

---
+++ SoftUni/Java/Fundamentals/03. Arrays/Exercise/05. TopIntegers.java
@@ -0,0 +1,30 @@
+package Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TopIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbersArray = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        for (int i = 0; i < numbersArray.length; i++) {
+            boolean isBigger = true;
+
+            for (int j = i + 1; j < numbersArray.length; j++) {
+                if (numbersArray[i] <= numbersArray[j]) {
+                    isBigger = false;
+                    break;
+                }
+            }
+
+            if (isBigger){
+                System.out.print(numbersArray[i] + " ");
+            }
+        }
+    }
+}
