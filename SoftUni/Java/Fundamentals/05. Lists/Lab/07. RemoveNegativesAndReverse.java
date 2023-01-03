762047a8bb8c035f3dcf3e69389ce84650e7f64e
pechev <pechev95@gmail.com>
2023-01-03 22:16:22 +0200

    Add exercise Remove negatives and reverse

---
+++ SoftUni/Java/Fundamentals/05. Lists/Lab/07. RemoveNegativesAndReverse.java
@@ -0,0 +1,30 @@
+package Lab;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class RemoveNegativesAndReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .filter(e -> e > 0)
+                .collect(Collectors.toList());
+
+        // Can be used also:
+        // numbers.removeIf(e -> e < 0);
+
+        Collections.reverse(numbers);
+
+        if (numbers.isEmpty()) {
+            System.out.println("empty");
+        } else {
+            System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
+        }
+    }
+}
