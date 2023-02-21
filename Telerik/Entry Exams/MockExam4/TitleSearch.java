dab45b62d7883d7e74768fe9737dfb348ca97123
pechev <pechev95@gmail.com>
2023-02-21 22:49:44 +0200

    Add exercise Title search

---
+++ Telerik/Entry Exams/MockExam4/TitleSearch.java
@@ -0,0 +1,11 @@
+package MockExam4;
+
+import java.util.*;
+
+public class TitleSearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
