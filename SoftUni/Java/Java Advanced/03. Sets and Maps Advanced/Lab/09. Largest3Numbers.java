2e4cec298dc7f21c74d096de988d6684b5294dbf
pechev <pechev95@gmail.com>
2023-03-07 20:20:11 +0200

    Add exercise Largest 3 Numbers

---
+++ SoftUni/Java/Java Advanced/03. Sets and Maps Advanced/Lab/09. Largest3Numbers.java
@@ -0,0 +1,21 @@
+package Lab;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Largest3Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted((n1, n2) -> n2.compareTo(n1))
+                .limit(3)
+                .collect(Collectors.toList());
+
+        numbers.forEach(number -> System.out.print(number + " "));
+    }
+}
