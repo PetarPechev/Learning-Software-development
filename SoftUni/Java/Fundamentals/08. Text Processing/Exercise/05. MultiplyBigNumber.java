04cc14842b2eb5997feb2e9a9d3f1fc8b693cdcc
pechev <pechev95@gmail.com>
2023-03-09 22:29:25 +0200

    Deleted unnecessary things in BigInteger reading

--- SoftUni/Java/Fundamentals/08. Text Processing/Exercise/05. MultiplyBigNumber.java
+++ SoftUni/Java/Fundamentals/08. Text Processing/Exercise/05. MultiplyBigNumber.java
@@ -7,7 +7,7 @@ public class MultiplyBigNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        BigInteger firstNum = new BigInteger(String.valueOf(scanner.nextLine()));
+        BigInteger firstNum = new BigInteger(scanner.nextLine());
         int secondNum = Integer.parseInt(scanner.nextLine());
         BigInteger result = firstNum.multiply(BigInteger.valueOf(secondNum));
 
