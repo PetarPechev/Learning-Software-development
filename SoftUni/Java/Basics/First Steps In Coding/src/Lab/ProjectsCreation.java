220a00816d7dcd5ec66312455908942c3cb8db96
Petar Pechev <pechev95@gmail.com>
2023-04-10 19:23:23 +0300

    add java first steps

---
+++ SoftUni/Java/Basics/First Steps In Coding/src/Lab/ProjectsCreation.java
@@ -0,0 +1,16 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class ProjectsCreation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String architectName = scanner.nextLine();
+        int projectsCount = Integer.parseInt(scanner.nextLine());
+        int totalHoursNeeded = projectsCount * 3;
+
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.",
+                architectName,totalHoursNeeded, projectsCount);
+    }
+}
