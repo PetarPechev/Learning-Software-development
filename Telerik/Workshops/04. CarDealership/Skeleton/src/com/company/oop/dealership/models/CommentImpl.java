3767946d688ea2de56597ad01d42e7a44f08aff3
Petar Pechev <pechev95@gmail.com>
2023-04-24 17:00:35 +0300

    Add workshop 3 - Car Dealership

---
+++ Telerik/Workshops/04. CarDealership/Skeleton/src/com/company/oop/dealership/models/CommentImpl.java
@@ -0,0 +1,27 @@
+package com.company.oop.dealership.models;
+
+import com.company.oop.dealership.models.contracts.Comment;
+
+import static java.lang.String.format;
+
+public class CommentImpl implements Comment {
+
+    public static final int CONTENT_LEN_MIN = 3;
+    public static final int CONTENT_LEN_MAX = 200;
+    private static final String CONTENT_LEN_ERR = format(
+            "Content must be between %d and %d characters long!",
+            CONTENT_LEN_MIN,
+            CONTENT_LEN_MAX);
+
+    @Override
+    public String getContent() {
+        return null;
+    }
+
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+
+    //TODO
+}
